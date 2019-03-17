c73cf6ed14bad0a9cfe15f9b016cdaf81decf1a8
Ian Cook Westgate <iwestgate@hotmail.com>
2019-03-17 10:49:37 -0700

    separate user interface and business logic.

---
+++ src/business-logic.js
@@ -0,0 +1,7 @@
+export class newClass {
+
+  constructor(classy) {
+    this.classy= classy;
+  }
+
+}
--- src/user-interface-logic.js
+++ src/user-interface-logic.js
@@ -2,7 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import './scss/styles.scss';
+import { newClass } from "./business-logic.js";
 
 $(document).ready(function() {
-  console.log("hey, you should buy Abe a beer sometime!");
+  console.log("hey, you should buy Ian a beer sometime!");
 });
